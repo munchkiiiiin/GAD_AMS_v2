50807f5111e002eb018d6bfc21491e9a11b9292e
munchkiiiiin <munchkiiiiin@users.noreply.github.com>
2026-09-06 11:35:12 +0000

    deploy: 6e855a4026dfc5ff3488ee7b668da40c7938011c

---
+++ assets/UserManualView-BSvrutzb.js
@@ -0,0 +1,1 @@
+import{M as e,S as t}from"./vendor-vue-Bec1agld.js";import{t as n}from"./UserManualUnified-V7sJrrb8.js";var r={__name:`UserManualView`,setup(r){return(r,i)=>(e(),t(n,{role:`admin`}))}};export{r as default};
