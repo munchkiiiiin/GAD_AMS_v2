02ca9edb69a058da238dce3e51cdd970cf502e02
munchkiiiiin <munchkiiiiin@users.noreply.github.com>
2026-09-10 12:55:34 +0000

    deploy: 9d7f36d6ba5536773530d6a8980edde823dee022

--- index.php
+++ index.php
@@ -98,14 +98,14 @@ if ($isBot && preg_match('/^\/gad-corner\/([0-9]+)/', $_SERVER['REQUEST_URI'], $
       window.dispatchEvent(new Event('pwa-prompt-available'));
     });
   </script>
-  <script type="module" crossorigin src="/assets/index-Dmk3BuvU.js"></script>
+  <script type="module" crossorigin src="/assets/index-Aik4Iy4V.js"></script>
   <link rel="modulepreload" crossorigin href="/assets/rolldown-runtime-S-ySWqyJ.js">
   <link rel="modulepreload" crossorigin href="/assets/vendor-libs-CtKfe6SA.js">
   <link rel="modulepreload" crossorigin href="/assets/vendor-charts-BlWJbwSF.js">
   <link rel="modulepreload" crossorigin href="/assets/vendor-vue-BlhUUiYR.js">
   <link rel="stylesheet" crossorigin href="/assets/vendor-libs-_QaLSP8Q.css">
   <link rel="stylesheet" crossorigin href="/assets/vendor-vue-D7vsgEFT.css">
-  <link rel="stylesheet" crossorigin href="/assets/index-DowRzPnA.css">
+  <link rel="stylesheet" crossorigin href="/assets/index-Brbk2k4g.css">
 </head>
 
 <body>
